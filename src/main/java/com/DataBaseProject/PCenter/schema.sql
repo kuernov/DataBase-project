71e13c65c69248e3cdf5ccdebd6246e4deff9c38
Bartosz Małyjasiak <bartosz.malyjasiak@gmail.com>
2023-11-05 15:37:21 +0100

    add liquibase configuration

---
+++ src/main/java/com/DataBaseProject/PCenter/schema.sql
@@ -0,0 +1,8 @@
+create table clients(
+                        id serial not null primary key,
+                        name varchar not null,
+                        lastName varchar not null,
+                        email varchar not null,
+                        phone varchar not null,
+                        address varchar not null
+);
---
+++ src/main/resources/database/clients.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset malyjasiak:2
+create table clients(
+                        id serial not null primary key,
+                        name varchar not null,
+                        lastName varchar not null,
+                        email varchar not null,
+                        phone varchar not null,
+                        address varchar not null
+);
